989a45dddb49fb11d1817d9133ff2ef15c604012
JDunc <jjsduncan@gmail.com>
2023-04-21 20:31:54 -0400

    add header, search form and buttons to html

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// var apiKey = "189c818c0cb64734ca920298a295b669";
+
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+// var city; 
+
+
+// fetch(queryURL)
+
+
+
+
+
+
+console.log("hello");
+
+//Need variable to accept user city input
+
+//Likely specify state and country variables in API call/latitude, longitude. 

d894290f713e996c59443c381649ddf48e2de500
JDunc <jjsduncan@gmail.com>
2023-04-25 17:04:23 -0400

    complete code for geocoding api call, add functions for current weather and forecast API calls

--- assets/script.js
+++ assets/script.js
@@ -9,27 +9,52 @@
 
 
 //Homework Setup Instructions:
-
+var historyArr =[];
 
 //First function call to get lat/lon from Geocoding API call
 function getCity (inputFromField) {
-    var url = "http://api.openweathermap.org/geo/1.0/direct?" + inputFromField + "&limit=1&appid=189c818c0cb64734ca920298a295b669"
-        fetch(url, {
-            method: 'GET',
-        })
+    var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputFromField + "&limit=1&appid=189c818c0cb64734ca920298a295b669"
+        fetch(url)
         .then(function (data) {
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-            getWeatherCurrent(lat,lon);
+            return data.json()
         })
+        .then(function(response) {
+            console.log(response)
+            var lat = response[0].lat;
+            var lon = response[0].lon;
+            getWeatherCurrent(lat,lon);
+            getForecastCurrent(lat, lon);
+        })
+        console.log("in function")
 };
 
 function getWeatherCurrent (lat, lon) {
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=189c818c0cb64734ca920298a295b669`
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=189c818c0cb64734ca920298a295b669`)
+    .then(firstResponse => firstResponse.json())
+    .then( (secondResponse) => {
+        console.log(secondResponse)
+        //daily weather
+        document.querySelector(".temp").textContent = secondResponse.main.temp + " F°";
+    })
+}
+
+function getForecastCurrent (lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=189c818c0cb64734ca920298a295b669`)
+    .then(firstResponse => firstResponse.json())
+    .then( (secondResponse) => {
+        console.log(secondResponse)
+        // fivev day
+    })
 }
 
 
+document.getElementById("submitForm").addEventListener("submit", function(e) {
+    e.preventDefault();
 
+    var cityIn = document.getElementById("cityInput").value;
+    historyArr.push(cityIn)
+    getCity(cityIn)
+})
 
 
 

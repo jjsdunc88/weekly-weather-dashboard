da94429073d515037430498c6291cfd87b3c18b7
JDunc <jjsduncan@gmail.com>
2023-04-27 18:15:36 -0400

    format docs

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,49 @@
 // var apiKey = "189c818c0cb64734ca920298a295b669"; - API key for openweathermap.org
 
 
-var historyArr =[];
+var historyArr = [];
 
 //First function call to get lat/lon from Geocoding API call
-function getCity (inputFromField) {
+function getCity(inputFromField) {
     var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputFromField + "&limit=1&appid=189c818c0cb64734ca920298a295b669"
-        fetch(url)
+    fetch(url)
         .then(function (data) {
             return data.json()
         })
-        .then(function(response) {
+        .then(function (response) {
             console.log(response)
             var lat = response[0].lat;
             var lon = response[0].lon;
-            getWeatherCurrent(lat,lon);
+            getWeatherCurrent(lat, lon);
             getForecastCurrent(lat, lon);
         })
-        console.log("in function")
+    console.log("in function")
 };
 
 
 //Current Weather data API call
-function getWeatherCurrent (lat, lon) {
+function getWeatherCurrent(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=189c818c0cb64734ca920298a295b669`)
-    .then(firstResponse => firstResponse.json())
-    .then( (secondResponse) => {
-        console.log(secondResponse)
-        //daily weather
-        document.querySelector(".temp").textContent = secondResponse.main.temp + " F°";
-    })
+        .then(firstResponse => firstResponse.json())
+        .then((secondResponse) => {
+            console.log(secondResponse)
+            //daily weather
+            document.querySelector(".temp").textContent = secondResponse.main.temp + " F°";
+        })
 }
 
 
 //5 day / 3 hour forecast data
-function getForecastCurrent (lat, lon) {
+function getForecastCurrent(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=189c818c0cb64734ca920298a295b669`)
-    .then(firstResponse => firstResponse.json())
-    .then( (secondResponse) => {
-        console.log(secondResponse)
-    })
+        .then(firstResponse => firstResponse.json())
+        .then((secondResponse) => {
+            console.log(secondResponse)
+        })
 }
 
 //Event Listener for City input form
-document.getElementById("submitForm").addEventListener("submit", function(e) {
+document.getElementById("submitForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
     var cityIn = document.getElementById("cityInput").value;
@@ -53,5 +53,5 @@ document.getElementById("submitForm").addEventListener("submit", function(e) {
 
 
 
-console.log("hello");
+
 
